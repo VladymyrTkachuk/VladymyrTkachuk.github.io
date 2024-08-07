c548e75ef8bc7a58ada120a425516b594de0bd64
Vladymyr <tvvist@ukr.net>
2024-08-07 13:44:52 +0300

    github

--- food/js/modules/cards.js
+++ food/js/modules/cards.js
@@ -43,7 +43,7 @@ function cards() {
 
   // getResource("http://localhost:3000/menu").then((data) => {
   getResource(
-    "https://github.com/VladymyrTkachuk/VladymyrTkachuk.github.io/tree/main/food/db.json/menu"
+    "https://VladymyrTkachuk.github.io/tree/main/food/db.json/menu"
   ).then((data) => {
     data.forEach(({ img, altimg, title, descr, price }) => {
       new MenuCard(
